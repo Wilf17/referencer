faf1cdda9b3a5207fdb16e865390ea5402c815b8
Wilf17 <lihounhintowilfrid.youngsavage@gmail.com>
2022-11-29 12:13:35 +0100

    Ajout de l'interface de recherche du patient dans la BDD avant toute inscription et modification de quelques détails sur l'acceuil

---
+++ assets/js/referencement.js
@@ -0,0 +1,29 @@
+var suivant1=document.getElementById("suivant1");
+var suivant2=document.getElementById("suivant2");
+var precedent1=document.getElementById("precedent1");
+var precedent2=document.getElementById("precedent2");
+var section1=document.getElementById("infospatients");
+var section2=document.getElementById("infosconstantes");
+var section3=document.getElementById("infospathologies");
+
+suivant1.addEventListener("click",function(){
+   section1.style.display="none";
+   section2.style.display="grid";
+   section3.style.display="none";
+},false);
+precedent1.addEventListener("click",function(){
+   section1.style.display="grid";
+   section2.style.display="none";
+   section3.style.display="none";
+},false)
+suivant2.addEventListener("click",function(){
+  section1.style.display="none";
+  section2.style.display="none";
+  section3.style.display="grid";
+  
+},false)
+precedent2.addEventListener("click",function(){
+section1.style.display="none";
+section2.style.display="grid";
+section3.style.display="none";
+},false)
